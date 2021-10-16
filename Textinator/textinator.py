42941a95b94bfa78b5d0f16cc6977d87cc42891e
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2021-10-16 17:40:05 +0100

    Add files via upload

--- Textinator/textinator.py
+++ Textinator/textinator.py
@@ -1,3 +1,4 @@
+import os
 import time
 import win32com.client
 from pywinauto import application
@@ -8,18 +9,29 @@ shell = win32com.client.Dispatch("WScript.Shell")
 var_0 = ''
 
 
+def clear_console():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):  # If Machine is running on Windows, use cls
+        command = 'cls'
+    os.system(command)
+
+
 def funk_0():
     global var_0
-
-    time.sleep(3)
+    print('-- time remaining: 3 second')
+    time.sleep(1)
+    print('-- time remaining: 2 second')
+    time.sleep(1)
+    print('-- time remaining: 1 second')
+    time.sleep(1)
     shell.SendKeys(var_0)
 
 
 while True:
+    clear_console()
     print('-'*100)
     print(' '*44 + 'TEXTINATOR')
     print('\nCircumvents password input limitations where input may be restricted.')
     print('\nUpon hitting enter you will have 3 seconds to select the text field in which to output data.')
     var_0 = input('\nEnter text to output:')
     funk_0()
-
